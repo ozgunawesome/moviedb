e7ebaf1c4ff834f3f39e596481846a84a179bfe6
Özgün Ayaz <ozgunayaz@gmail.com>
2017-06-25 16:08:54 +0200

    Make the database persistent

---
+++ moviedb/src/main/java/cc/ozgun/moviedb/configuration/MovieRepositoryConfiguration.java
@@ -0,0 +1,19 @@
+package cc.ozgun.moviedb.configuration;
+
+import cc.ozgun.moviedb.model.Movie;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+
+/**
+ * Created by Özgün Ayaz on 6/25/2017. License: CC BY-SA 4.0
+ */
+@Configuration
+public class MovieRepositoryConfiguration extends RepositoryRestMvcConfiguration {
+
+    @Override
+    public RepositoryRestConfiguration config() {
+        return super.config().exposeIdsFor(Movie.class); //expose the 'id' field
+    }
+}
+
